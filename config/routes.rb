33a7f80469c1c67f3fadf5ce6420695669113695
Grace Jang <hygracem@gmail.com>
2020-09-24 20:03:55 +0900

    up to controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root "cocktails#index"
+  resources :cocktails, only: [ :index, :create, :new, :show ] do
+    resources :doses, only: [ :new, :create ]
+  end
+  resources :doses, only: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
