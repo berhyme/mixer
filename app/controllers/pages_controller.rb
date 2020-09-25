a6361e656e76b7fab9899eef70fffffa37f575f8
Grace Jang <hygracem@gmail.com>
2020-09-25 19:18:39 +0900

    home view button edit and form alignment

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   def home
     @cocktails = Cocktail.all
+    @search = params["search"]
+    if @search.present?
+      @name = @search["name"]
+      @cocktails = Cocktail.where(name: @name)
+    end
   end
 end
